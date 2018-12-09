2459f7814cbd68eaad2f8428ed5cd64425834810
Lucas Gyan Coloda <37116457+Lucas-Coloda@users.noreply.github.com>
2018-12-09 16:54:50 -0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+# author: lucas Gyan Coloda
+# podemos descobrir a próxima raiz se somarmos o número impar sequencial
+
+w = 0
+x = 2
+y = 0
+z = 0
+print("raiz de =|  | some num impar(= a proxima raiz de)")
+while w <= 100:
+    y = x * x
+    w = w + 1
+    z = 2 * x + 1
+    print(y, x, z)
+    x = x + 1
